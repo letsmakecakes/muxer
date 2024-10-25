5cd510a4f131c96f1231a5e723777cfc1a210178
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-25 21:24:04 +0530

    Added method to start TCP server, accepting connections and reading messages

--- internal/transport/tcp.go
+++ internal/transport/tcp.go
@@ -1,7 +1,10 @@
 package transport
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -19,4 +22,35 @@ func NewTCPSourceHandler(host string, port int) (*TCPSourceHandler, error) {
 		return nil, err
 	}
 	return &TCPSourceHandler{address: address, listener: listener}, nil
+}
+
+// Listen starts the TCP server, accepting connections and reading messages.
+func (t *TCPSourceHandler) Listen(ctx context.Context, forwardFunc func([]byte)) error {
+	defer t.listener.Close()
+
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			log.Printf("TCP accept error: %v\n", err)
+			continue
+		}
+
+		go func(c net.Conn) {
+			defer c.Close()
+
+			scanner := bufio.NewScanner(c)
+			for scanner.Scan() {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					message := scanner.Bytes()
+					forwardFunc(message) // Forward the received message to muxer
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				log.Printf("Error reading from TCP connection: %v\n", err)
+			}
+		}(conn)
+	}
 }
